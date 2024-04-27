0b6d25af1195316e9169086b7766723f0ee9510d
Alexey Lavrenchuk <alexey@code-care.pro>
2024-04-28 02:26:38 +0300

    add middlewere, change get users, add post users

--- server.js
+++ server.js
@@ -5,6 +5,17 @@ const mysql = require('mysql2');
 
 const app = express();
 
+// Middleware for parsing JSON and URL-encoded data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 const dbConfig = {
   host: process.env.DATABASE_URL,
   user: process.env.DB_USER,
@@ -41,8 +52,8 @@ connection.connect((err) => {
       const createTableQuery = `
         CREATE TABLE IF NOT EXISTS users (
           id INT AUTO_INCREMENT PRIMARY KEY,
-          userName VARCHAR(255) NOT NULL,
-          score VARCHAR(255) NOT NULL,
+          userName VARCHAR(255) NOT NULL UNIQUE,
+          score INT NOT NULL DEFAULT 0
         )
       `;
       connection.query(createTableQuery, (err) => {
@@ -62,9 +73,9 @@ app.use((err, req, res, next) => {
   res.status(500).send('Something went wrong!');
 });
 
-// Processing queries to retrieve data from a table
+// Processing queries to retrieve data from a table (only first 10 records sorted by score descending)
 app.get('/users', (req, res) => {
-  const selectQuery = 'SELECT * FROM users';
+  const selectQuery = 'SELECT * FROM users ORDER BY score DESC LIMIT 10';
   connection.query(selectQuery, (err, results) => {
     if (err) {
       console.error('Request execution error:', err);
@@ -74,6 +85,49 @@ app.get('/users', (req, res) => {
     res.json(results);
   });
 });
+
+app.post('/users', (req, res) => {
+  const { userName, score } = req.body;
+
+  console.log("From server", userName, score)
+  if (!userName || isNaN(score)) {
+    res.status(400).json({ error: 'User data invalid' });
+    return;
+  }
+
+  // Check if a user with a given name exists
+  connection.query('SELECT * FROM users WHERE userName = ?', [userName], (err, results) => {
+    if (err) {
+      console.error('Request execution error:', err);
+      res.status(500).json({ error: 'Request execution error' });
+      return;
+    }
+
+    if (results.length === 0) {
+      // If there is no user with the same name, perform the INSERT operation
+      connection.query('INSERT INTO users (userName, score) VALUES (?, ?)', [userName, score], (err, results) => {
+        if (err) {
+          console.error('Request execution error:', err);
+          res.status(500).json({ error: 'Request execution error' });
+          return;
+        }
+        res.json(results);
+      });
+    } else {
+      // If a user with the same name already exists, perform the UPDATE operation
+      connection.query('UPDATE users SET score = ? WHERE userName = ?', [score, userName], (err, results) => {
+        if (err) {
+          console.error('Request execution error:', err);
+          res.status(500).json({ error: 'Request execution error' });
+          return;
+        }
+        res.json(results);
+      });
+    }
+  });
+});
+
+
 
 // Start the server
 const PORT = process.env.PORT || 3000;
